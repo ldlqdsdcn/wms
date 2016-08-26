d4a5c2164b3b937d7b11cea30f5bc714b416b999
solomon.liu <ldlqdsdcn@gmail.com>
2016-08-26 23:31:33 +0800

    添加标签类

---
+++ web/src/main/java/com/delmar/core/web/action/LabelAction.java
@@ -0,0 +1,118 @@
+/******************************************************************************
+ * 德玛国际物流有限公司  2013-07-01												  *
+ *	作者：刘大磊								                                              *
+ * 电话：0532-66701118                                                                * 
+ * email:liua@delmarchina.com						                              *
+ *****************************************************************************/
+package com.delmar.core.web.action;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.delmar.core.web.action.CoreEditPrivAction;
+
+import com.delmar.core.model.Label;
+
+import com.delmar.core.service.LabelService;
+
+
+/**
+ * @author 刘大磊 2016-08-26 23:24:10
+ */
+public class LabelAction extends CoreEditPrivAction {
+	private Label  label;
+	
+	@Autowired
+	private LabelService labelService;
+	
+	private void init()
+	{
+
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.delmar.core.web.action.CoreEditPrivAction#getModuleName()
+	 */
+	@Override
+	public String getModuleName() {
+		return "label";
+	}
+
+	/* (non-Javadoc)
+	 * @see com.delmar.core.web.action.CoreEditPrivAction#delete()
+	 */
+	@Override
+	public String delete() {
+		labelService.deleteByPrimaryKey(label.getId());
+		return list();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.delmar.core.web.action.CoreEditPrivAction#deleteList(java.lang.Integer[])
+	 */
+	@Override
+	public void deleteList(Integer[] ids) {
+		
+		labelService.deleteLabelList(ids);
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.delmar.core.web.action.CoreEditPrivAction#getModelId()
+	 */
+	@Override
+	public Integer getModelId() {
+
+		return label.getId();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.delmar.core.web.action.CoreEditPrivAction#editForm()
+	 */
+	@Override
+	public void editForm() {
+		 label= labelService.selectByPrimaryKey(id);
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.delmar.core.web.action.CoreEditPrivAction#search()
+	 */
+	@Override
+	public List search() {
+		return labelService.selectByExample(null);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.delmar.core.web.action.CoreEditPrivAction#createForm()
+	 */
+	@Override
+	public void createForm() {
+		label=new Label();
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.delmar.core.web.action.CoreEditPrivAction#saveForm()
+	 */
+	@Override
+	public String saveForm() {
+
+		labelService.save(label);
+		return "edit";
+	}
+	/**
+	 * @return the usergroup
+	 */
+	public Label getLabel() {
+		return label;
+	}
+
+	/**
+	 * @param label the label to set
+	 */
+	public void setLabel(Label label) {
+		this.label = label;
+	}
+
+}
