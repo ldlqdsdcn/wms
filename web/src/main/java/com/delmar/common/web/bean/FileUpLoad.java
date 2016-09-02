dd5e4e8357c20ddb0a8b2d34d49b679505dc9ef7
solomon.liu <ldlqdsdcn@gmail.com>
2016-09-02 21:52:07 +0800

    修改版权声明

--- web/src/main/java/com/delmar/common/web/bean/FileUpLoad.java
+++ web/src/main/java/com/delmar/common/web/bean/FileUpLoad.java
@@ -22,7 +22,7 @@ import com.delmar.core.web.bean.EaContext;
 import com.delmar.core.web.util.FacesUtils;
 import com.google.gson.Gson;
 /******************************************************************************
- * 刘大磊  2013-07-01												  *
+ * 版权所有 刘大磊 2013-07-01												  *
  *	作者：刘大磊								                                              *
  * 电话：13336390671                                                               * 
  * email:ldlqdsd@126.com						                              *
